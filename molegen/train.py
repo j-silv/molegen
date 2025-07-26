e4ccec1cdad5f330c09c6bd8d3839a904b5fb9a4
Justin Silver <justinrsilver@gmail.com>
2025-07-25 17:04:36 -0700

    Add rest of training loop and verify we can overfit
    Training loss steadily decreases and the predictions match after
    100 epochs. Loss goes down to 0.4 roughly. Great!
    
    Next step is to add the bond generation

--- molegen/train.py
+++ molegen/train.py
@@ -34,11 +34,16 @@ def main():
     embd = 16 # embedding size of a vocab indice
     vocab_size = len(a2t)
     num_layers = 1     # number of GCN layers
+    lr = 0.001
+    betas = (0.9, 0.999)
+    eps = 1e-08
+    epochs = 100
     ###################################################
     
     model = MoleGen(vocab_size=vocab_size, num_layers=num_layers, embd=embd, max_atoms=max_atoms)
     
-
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=betas, eps=eps)
+    loss_fn = torch.nn.CrossEntropyLoss()
 
     print(model)
 
@@ -46,8 +51,26 @@ def main():
     # result = model(data)
     
     model.eval()
-    for idx, batch in enumerate(loader):
-        boa, z = model(batch)
+    for epoch in range(epochs):
+        loss_avg = 0.0
+        
+        for idx, batch in enumerate(loader):
+            boa, z = model(batch)
+            
+            # import code; code.interact(local=locals())
+            
+            optimizer.zero_grad()
+            
+            # we have to permute because loss function expects (N, C, d1, d2, dK)
+            # and we have a K-dimensional loss here
+            loss = loss_fn(torch.permute(boa, (0, 2, 1)), batch.y.long())
+            loss_avg += loss.item()
+            
+            loss.backward()
+            
+            optimizer.step()
+            
+        print(f"Avg loss: {loss_avg/len(loader):.5f} | Epoch {epoch}")
         
         for token, count in enumerate(batch.y[0]):
             print(f"{t2a[token]:<2}({count:>3}) ", end="")
@@ -57,9 +80,7 @@ def main():
             count = torch.argmax(logits)
             print(f"{t2a[token]:<2}({count:>3}) ", end="")
         print("   PREDICTED (1st graph) - batch", idx)  
-        
-        
-        # import code; code.interact(local=locals())
+    
     
     
 
