5c136c0437a332c3ab6a401ce78f2345f4467929
Justin Silver <justinrsilver@gmail.com>
2025-08-30 10:45:43 -0700

    Remove png file since it is already hosted on my portfolio website

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 """
 # MoleGen - generative ML model for molecules
 
-[![Molegen architecture](./flowchart.png)](https://www.justin-silver.com/posts/molegen/)
+[![Molegen architecture](https://www.justin-silver.com/assets/img/molegen/flowchart.png)](https://www.justin-silver.com/posts/molegen/)
 
 This site hosts a replicated model from the 2019 ML paper: [A Two-Step Graph Convolutional Decoder for Molecule Generation](https://arxiv.org/pdf/1906.03412). Here you will find various interactive widgets for visualizing model inputs/outputs, and for running inference. 
 
