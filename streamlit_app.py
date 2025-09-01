a80d42a85fcc6429de97aa158f52aaa1abb6a03c
Justin Silver <justinrsilver@gmail.com>
2025-09-01 15:16:37 -0700

    Add nav bar to top so that it takes up less space when embed

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 home_page = st.Page("pages/home.py", title="Home", default=True)
 data_page = st.Page("pages/data.py", title="ZINC dataset exploration")
-pg = st.navigation([home_page, data_page])
+pg = st.navigation([home_page, data_page], position='top')
 
 st.set_page_config(page_title="MoleGen")
 
